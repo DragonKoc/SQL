37dde218f63f0fc5e3a7b0a653b3b82e32d3536c
Koc Grebenyuk <kgrebeniuk@gmail.com>
2018-11-14 15:02:55 +0200

    1.2
    second change

--- alter.sql
+++ alter.sql
@@ -3,3 +3,4 @@ alter table LAYEDCONDLOG
 alter table LAYEDCONDLOG 
   add constraint FK_CONDLOG_TO_ACCMODELS foreign key (ACTMODID) 
   references Accmodelclass (AccModelID);
+  select * from T_ISO_CONFIG;
