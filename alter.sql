c819a580df30351e7e8cde32961627fe1b684ce5
Koc Grebeniuk <kgrebeniuk@gmail.com>
2018-11-14 21:56:37 +0200

    i#
    last change
    commit

--- alter.sql
+++ alter.sql
@@ -4,3 +4,9 @@ alter table LAYEDCONDLOG
   add constraint FK_CONDLOG_TO_ACCMODELS foreign key (ACTMODID) 
   references Accmodelclass (AccModelID);
   select * from T_ISO_CONFIG;
+select 1 from dual;
+
+
+
+
+select num from acc_tab where accid >= 100;
