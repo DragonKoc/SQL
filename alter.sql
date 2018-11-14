bf3bc209cef2a257df8f380ba025e39063895af8
Koc Grebenyuk <kgrebeniuk@gmail.com>
2018-11-14 15:00:23 +0200

    1.1
    first change

---
+++ alter.sql
@@ -0,0 +1,5 @@
+alter table LAYEDCONDLOG 
+  drop constraint FK_CONDLOG_TO_ACCMODES; 
+alter table LAYEDCONDLOG 
+  add constraint FK_CONDLOG_TO_ACCMODELS foreign key (ACTMODID) 
+  references Accmodelclass (AccModelID);
